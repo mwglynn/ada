8cb00f1e4ee0eb963f22c2c82703e65d1ca13362
Mackenzie Glynn <mail@mglynn.net>
2018-10-17 19:11:55 -0700

    Testing and verified reading

--- src/AdaMain.java
+++ src/AdaMain.java
@@ -4,8 +4,16 @@ public class AdaMain {
     public static void main(String[] args) {
         TCPSocketClient client = new TCPSocketClient("localhost", 6259);
         TCPMessageSender sender = new TCPMessageSender(client);
+        TCPMessageReader reader = new TCPMessageReader(client);
         sender.SendMessage("Test");
+
+        String s = null;
+        while (s == null) {
+            s = reader.ReadMessage();
+        }
+        System.out.println(s);
         sender.Close();
+        reader.Close();
         client.Close();
     }
 }
