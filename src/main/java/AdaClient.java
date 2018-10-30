b6533e170619eb8e2292171a62d079061d3d01d0
rivatal <riva.tropp@mail.yu.edu>
2018-10-30 03:53:31 -0400

    Test 2

--- src/main/java/AdaClient.java
+++ src/main/java/AdaClient.java
@@ -1,53 +1,50 @@
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Optional;
+import java.util.Scanner;
 
 /** {@code AdaClient} reads messages from and sends Messages to {@link AdaServer}. */
 public class AdaClient {
 
-    private static final int port = 6259;
+  private static final int port = 6259;
 
-    public static void main(String[] args) {
-        NetworkSocketClient client = new NetworkSocketClient("localhost", port);
-        NetworkSender sender = new NetworkSender(client);
-        NetworkReader reader = new NetworkReader(client);
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    NetworkSocketClient client = new NetworkSocketClient("localhost", port);
+    NetworkSender sender = new NetworkSender(client);
+    NetworkReader reader = new NetworkReader(client);
+    Scanner input = new Scanner(System.in);
 
-        Thread sendMessages = new Thread(() -> {
-            while (true) {
+    Thread sendMessages =
+        new Thread(
+            () -> {
+              while (true) {
                 if (input.hasNext()) {
-                    sender.SendMessage(input.nextLine());
+                  sender.SendMessage(input.nextLine());
                 } else {
-                    input.nextLine();
+                  input.nextLine();
                 }
-            }      
-        });
-        sendMessages.start();
-        
-        Optional<String> s = Optional.empty();
-        do {
-            s = reader.ReadMessage();
-            if (s.isPresent()) {
-                System.out.println("-" + s.get());
-                if(s.get().equals("exit")) {
-                    break;
-                }
-            }
-        } while (true);
+              }
+            });
+    sendMessages.start();
 
-
-        /* clean exit */
-        sender.Close();
-        reader.Close();
-        client.Close();
-
-        try {
-            sendMessages.join();
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
+    Optional<String> s = Optional.empty();
+    do {
+      s = reader.ReadMessage();
+      if (s.isPresent()) {
+        System.out.println("-" + s.get());
+        if (s.get().equals("exit")) {
+          break;
         }
+      }
+    } while (true);
+
+    /* clean exit */
+    sender.Close();
+    reader.Close();
+    client.Close();
+
+    try {
+      sendMessages.join();
+    } catch (InterruptedException ie) {
+      ie.printStackTrace();
     }
+  }
 }
--- src/main/java/AdaServer.java
+++ src/main/java/AdaServer.java
@@ -1,21 +1,23 @@
 /** AdaServer houses the main for our Chat Server. */
 public class AdaServer {
 
-    private static final int port = 6259;
+  private static final int port = 6259;
 
-    public static void main(String[] args) {
-        TCPHost host = new TCPHost(port);
+  public static void main(String[] args) {
+    TCPHost host = new TCPHost(port);
 
-        Thread hostThread = new Thread(() -> {
-            while (host.Tick());
-            host.Close();
-        });
-        hostThread.start();
+    Thread hostThread =
+        new Thread(
+            () -> {
+              while (host.Tick()) ;
+              host.Close();
+            });
+    hostThread.start();
 
-        try {
-            hostThread.join();
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
-        }
+    try {
+      hostThread.join();
+    } catch (InterruptedException ie) {
+      ie.printStackTrace();
     }
+  }
 }
