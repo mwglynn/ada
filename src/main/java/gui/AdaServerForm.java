ac18f9822fa5b08ba64e69cf64be987bc0426c2d
HDCP <theaccountname@yahoo.com>
2018-11-17 11:05:10 -0500

    Server Main Panel

--- src/main/java/gui/AdaServerForm.java
+++ src/main/java/gui/AdaServerForm.java
@@ -1,18 +1,30 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AdaServerForm {
     private JButton starButton;
+    private JPanel serverPanel;
 
     public AdaServerForm() {
         starButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Starting server");
+                JOptionPane.showMessageDialog(null, "Server Started");
             }
         });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ada Server");
+        frame.setPreferredSize(new Dimension(200, 200));
+        frame.setContentPane(new AdaServerForm().serverPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
