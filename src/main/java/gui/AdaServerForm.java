49396e7d3a9d0d7cea06140ac94e0a9b683b0800
HDCP <theaccountname@yahoo.com>
2018-11-17 10:31:05 -0500

    Ada Server form

--- src/main/java/gui/AdaServerForm.java
+++ src/main/java/gui/AdaServerForm.java
@@ -1,4 +1,18 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class AdaServerForm {
+    private JButton starButton;
+
+    public AdaServerForm() {
+        starButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Starting server");
+            }
+        });
+    }
 }
