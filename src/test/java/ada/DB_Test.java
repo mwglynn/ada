d2c817282bc6de61be7ecbdb64d95efb1a674694
Nathan Reitinger <nathan.reitinger@columbia.edu>
2018-12-04 17:40:58 -0500

    works locally, testing travisCI

--- src/test/java/ada/DB_Test.java
+++ src/test/java/ada/DB_Test.java
@@ -35,71 +35,79 @@ public class DB_Test {
         try {
             Class.forName("org.postgresql.Driver");
             c = getConnection("jdbc:postgresql://localhost:5432",
-                    ".", "postgres");
+                    "postgres", "postgres");
 
         } catch (Exception e) {
             System.err.println(e);
         }
     }
 
-//    /** test user names (manually remove entries later)
-//     *
-//     */
-//    @Test
-//    public void testIllegalUsername() {
-//        String username = null;
-//        String flag = null;
-//
-//        username = "somthing'illegal'";
-//        String random = Double.toString(Math.random());
-//        username = username + random;
-//        flag = "n";
-//        new PostgreSQL_createUser();
-//        Boolean ret = PostgreSQL_createUser.main(username, flag);
-//        Assert.assertEquals(true, ret);
-//
-//    }
-//
-//    @Test
-//    public void testDoubleInsert() {
-//        String username = null;
-//        String flag = null;
-//
-//        username = "somthing'illegal'";
-//        String random = Double.toString(Math.random());
-//        username = username + random;
-//        flag = "n";
-//        new PostgreSQL_createUser();
-//        Boolean ret = PostgreSQL_createUser.main(username, flag);
-//        Assert.assertEquals(true, ret);
-//        ret = PostgreSQL_createUser.main(username, flag);
-//        Assert.assertEquals(false, ret);
-//    }
+    /** test user names (manually remove entries later)
+     *
+     */
+    @Test
+    public void userNameInsertion() {
+        /**
+         * this test tries to create a username with a special character
+         * the test should pass because we do not yet have restrictions
+         * on characters that may be used for usernames
+         *
+         * The test then checks a double insertion (trying to create a
+         * username that already exists) and should fail
+         */
+        String userName = null;
+        String host = "localhost";
+        String flag = null;
+        String random = Double.toString(Math.random());
 
-//    @Test
-//    public void testYesMatchName() {
-//        String username = null;
-//        String flag = null;
-//
-//        username = "somthing'illegal'";
-//        flag = "y";
-//        new PostgreSQL_createUser();
-//        Boolean ret = PostgreSQL_createUser.checkUser(username, flag);
-//        Assert.assertEquals(true, ret);
-//    }
+        userName = "l'sa";
+        userName = userName + random;
+        flag = "n";
+        PostgreSQLJDBC.InitPostgres("localhost");
+        new PostgreSQL_createUser();
+        Boolean ret = PostgreSQL_createUser.Create(host, userName, flag);
+        Assert.assertEquals(true, ret);
 
-//    @Test
-//    public void testNoMatchName() {
-//        String username = null;
-//        String flag = null;
-//
-//        String random = Double.toString(Math.random());
-//        username = random;
-//        flag = "y";
-//        new PostgreSQL_createUser();
-//        Boolean ret = PostgreSQL_createUser.checkUser(username, flag);
-//        Assert.assertEquals(false, ret);
-//    }
+        /* try to create the same username again */
+        ret = PostgreSQL_createUser.Create(host, userName, flag);
+        Assert.assertEquals(false, ret);
+
+    }
+
+
+    @Test
+    public void login_existingUser() {
+        /**
+         * test looks as using an existing username.
+         * the test first fails because the username does not exist
+         * then passes on inserting the new username
+         * then returns true for checking that the new username exists
+         */
+        String userName = null;
+        String host = "localhost";
+        String flag = null;
+        String random = Double.toString(Math.random());
+
+        userName = "katz";
+        userName = userName + random;
+        flag = "y";
+        new PostgreSQL_createUser();
+        Boolean ret = PostgreSQL_createUser.checkUser(host, userName, flag);
+        /* should fail because the user does not exist yet */
+        Assert.assertEquals(false, ret);
+
+        flag = "n";
+        new PostgreSQL_createUser();
+        ret = PostgreSQL_createUser.Create(host, userName, flag);
+        /* should pass now because the user is new */
+        Assert.assertEquals(true, ret);
+
+        flag = "y";
+        new PostgreSQL_createUser();
+        ret = PostgreSQL_createUser.checkUser(host, userName, flag);
+        /* should pass now because the user is new */
+        Assert.assertEquals(true, ret);
+    }
 
 //    /** insert illegal message
 //     *
@@ -128,10 +136,5 @@ public class DB_Test {
 //        PostgreSQL_insertChat.main(jobj, receiver);
 //
 //    }
-
-
-
-
-
 
 }
