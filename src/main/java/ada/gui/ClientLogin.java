c747cd82fee567fbbf038a65caaa3dfee128ffd4
HDCP <theaccountname@yahoo.com>
2018-12-04 18:38:35 -0500

    Client Form Login and Registration

--- src/main/java/ada/gui/ClientLogin.java
+++ src/main/java/ada/gui/ClientLogin.java
@@ -1,4 +1,19 @@
 package ada.gui;
 
+import javax.swing.*;
+
 public class ClientLogin {
+    private JPanel panel1;
+    private JButton buttonLogin;
+    private JButton buttonRegister;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Client Login");
+        frame.setContentPane(new ClientLogin().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+
 }
