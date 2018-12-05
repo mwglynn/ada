fb7b83275c8db41cbf9e1f777e541b66da49d395
riva <riva.tropp@mail.yu.edu>
2018-12-04 19:21:21 -0500

    whatevs

--- src/main/java/ada/postgresql/AdaDB.java
+++ src/main/java/ada/postgresql/AdaDB.java
@@ -12,10 +12,11 @@ public class AdaDB implements Closeable {
     private static String USER_TABLE;
     private static String CHAT_TABLE;
 
-    public AdaDB(String host, String table_name) {
+    public AdaDB(String host, String db_name) {
+        USER_TABLE = db_name + "usertable";
+        CHAT_TABLE = db_name + "chattable";
         HOST = host;
-        USER_TABLE = table_name + "User";
-        CHAT_TABLE = table_name + "Chat";
+        // TODO: Make resistant to caps.
         try {
             /* for now, connect to default postgres */
             Class.forName("org.postgresql.Driver");
@@ -31,25 +32,29 @@ public class AdaDB implements Closeable {
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
-                "jdbc:postgresql://" + HOST + ":5432", "postgres", "postgres");
+                "jdbc:postgresql://" + HOST + ":5432/", "postgres", "postgres");
     }
+
+    //  private Connection getTempConnection() throws SQLException {
+    //    return getConnection();
+    //  }
 
     public void initPostgres() {
         try (Connection connection = getConnection();
              Statement stmt = connection.createStatement()) {
             connection.setAutoCommit(false);
-            System.out.println("Opened database successfully");
-
             /* execute creation of user */
             String sql =
                     "CREATE TABLE IF NOT EXISTS "
                             + USER_TABLE
                             + " ("
                             + "ID SERIAL NOT NULL,"
-                            + "userName VARCHAR(255) UNIQUE NOT NULL,"
-                            + "PRIMARY KEY (ID, userName))";
+                            + " userName VARCHAR(255) UNIQUE NOT NULL,"
+                            + " PRIMARY KEY (ID, userName))";
+            System.out.println(sql);
             stmt.executeUpdate(sql);
-            System.out.println("- " + USER_TABLE + " executed");
+            connection.commit();
+            System.out.println("- " + USER_TABLE + " created");
 
             /* execute creation of chat */
             sql =
@@ -67,11 +72,25 @@ public class AdaDB implements Closeable {
                             + USER_TABLE
                             + "(userName) ON DELETE NO ACTION)";
             stmt.executeUpdate(sql);
-            stmt.close();
-            System.out.println("- " + CHAT_TABLE + " executed");
+            connection.commit();
+            System.out.println("- " + CHAT_TABLE + " created");
+            System.out.println(" SELECT current_database();");
+            ResultSet s = stmt.executeQuery(" SELECT current_database();");
+            while (s.next()) {
+                System.out.println("Next");
+                System.out.println(s.getString(1));
+                ;
+            }
 
+            //        System.out.println("SELECT * FROM testuser");
+            //        ResultSet r = stmt.executeQuery("\\dt;");
+            //        "SELECT * FROM pg_catalog.pg_tables;
+            //        while (r.next()) {
+            //          System.out.println("NExt");
+            //          System.out.println(r.getString(1));
+            //        }
         } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            e.printStackTrace();
             System.exit(1);
         }
         System.out.println("Tables created successfully");
@@ -137,6 +156,7 @@ public class AdaDB implements Closeable {
 
                 PreparedStatement ps = connection.prepareStatement(sql);
                 ps.setString(1, userName);
+                System.out.println(ps.toString());
                 ps.execute();
 
                 connection.commit();
@@ -149,41 +169,40 @@ public class AdaDB implements Closeable {
     }
 
     public boolean checkUser(String userName) {
-        String userExists = "";
         try (Connection connection = getConnection();
              Statement stmt = connection.createStatement()) {
             connection.setAutoCommit(false);
-            String sql = "select exists(select 1 from " + USER_TABLE + " where username=" + "?" + ");";
-
+            String sql = "select id from " + USER_TABLE + " WHERE userName = '?';";
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, userName);
-            ResultSet resultSet = ps.executeQuery();
 
-            while (resultSet.next()) {
-                userExists = resultSet.getString(1);
-            }
-            resultSet.close();
-            stmt.close();
-            connection.commit();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            //      ps.setString(2, userName);
+            //      System.out.println(ps.toString());
+            System.out.println(sql);
+            ResultSet resultSet = stmt.executeQuery(sql);
+            System.out.println(resultSet.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return userExists.equals("t");
+        //
+        //      while (resultSet.next()) {
+        //        return resultSet.getString(1).equals("t");
+        //      }
+        //      resultSet.close();
+        //      stmt.close();
+        //      connection.commit();
+        //    } catch (SQLException e) {
+        //      e.printStackTrace();
+        //    }
+        return false;
     }
 
     @VisibleForTesting
     void clear() {
         try (Connection connection = getConnection();
              Statement stmt = connection.createStatement()) {
-            connection.setAutoCommit(false);
-
-            String sql = "DROP TABLE IF EXISTS " + CHAT_TABLE + ", " + USER_TABLE + ";";
-            System.out.println("Dropping Table!" + sql);
-
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.execute();
-
-            connection.commit();
+            // TODO: FIX
+            stmt.execute("DROP TABLE " + CHAT_TABLE + ", " + USER_TABLE + ";");
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/test/java/ada/postgresql/AdaDbTest.java
+++ src/test/java/ada/postgresql/AdaDbTest.java
@@ -1,14 +1,10 @@
 package ada.postgresql;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.postgresql.util.PSQLException;
-
-import static java.sql.DriverManager.getConnection;
 
 @RunWith(JUnit4.class)
 public class AdaDbTest {
@@ -16,7 +12,6 @@ public class AdaDbTest {
     private static final String DATABASE_USERNAME = "postgres";
     private static final String DATABASE_PASSWORD = "postgres";
     private static final String TEST_HOST = "jdbc:postgresql://localhost:5432";
-    private static final String TEST_USER = "credence";
     private AdaDB TEST_DB;
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -24,41 +19,48 @@ public class AdaDbTest {
     // TODO: remove configuration tests and put config in flags.
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         TEST_DB = new AdaDB("localhost", "test");
-        TEST_DB.clear();
         TEST_DB.initPostgres();
     }
+
+    //    @Test
+    //    public void testInitPostgres() {
+    //        TEST_DB = new AdaDB("localhost", "test");
+    //        TEST_DB.initPostgres();
+    //    }
 
     /**
      * tests setup on your computer.
      */
-    @Test(expected = PSQLException.class)
-    public void getConnection_wrongPort_throwsPSQLException() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        getConnection("jdbc:postgresql://localhost:5555", DATABASE_USERNAME, DATABASE_PASSWORD);
-    }
-
-    @Test(expected = PSQLException.class)
-    public void test_wrongUser_throwsPasswordException() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        getConnection(TEST_HOST, ".", DATABASE_PASSWORD);
-    }
-
-    @Test
-    public void test_validUser_Succeeds() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        getConnection(TEST_HOST, DATABASE_USERNAME, DATABASE_PASSWORD);
-    }
-
-    //    /** test user names (manually remove entries later)
-    //     */
+    //    @Test(expected = PSQLException.class)
+    //    public void getConnection_wrongPort_throwsPSQLException() throws Exception {
+    //        Class.forName("org.postgresql.Driver");
+    //        getConnection("jdbc:postgresql://localhost:5555", DATABASE_USERNAME, DATABASE_PASSWORD);
+    //    }
+    //
+    //    @Test(expected = PSQLException.class)
+    //    public void test_wrongUser_throwsPasswordException() throws Exception {
+    //        Class.forName("org.postgresql.Driver");
+    //        getConnection(TEST_HOST, ".", DATABASE_PASSWORD);
+    //    }
+    //
+    //    @Test
+    //    public void test_validUser_Succeeds() throws Exception {
+    //        Class.forName("org.postgresql.Driver");
+    //        getConnection(TEST_HOST, DATABASE_USERNAME, DATABASE_PASSWORD);
+    //    }
+    //
+    //    //    /** test user names (manually remove entries later)
+    //    //     */
     @Test
     public void createUser_validUsername_succeeds() {
-        Assert.assertFalse(TEST_DB.checkUser(TEST_USER));
-        Assert.assertTrue(TEST_DB.createUser(TEST_USER));
+        Assert.assertFalse(TEST_DB.checkUser("credence"));
+        Assert.assertTrue(TEST_DB.createUser("credence"));
     }
 
+    //
+    //
     @Test
     public void createUser_crazySqlCharacters_succeeds() {
         String crazyUsername = "totally'legal";
@@ -66,41 +68,48 @@ public class AdaDbTest {
         Assert.assertTrue(TEST_DB.createUser(crazyUsername));
     }
 
+    //
     @Test
     public void test_createExistingUser_returnsFalse() {
-        Assert.assertTrue(TEST_DB.createUser(TEST_USER));
-        Assert.assertFalse(TEST_DB.createUser(TEST_USER));
+        Assert.assertTrue(TEST_DB.createUser("frerin"));
+        Assert.assertFalse(TEST_DB.createUser("frerin"));
     }
 
-    /**
-     * insert illegal message
-     */
+    //
+    //    /**
+    //     * insert illegal message
+    //     */
     @Test
     public void insertMessage_validSql_succeedsAndDoesNotDestroyDatabase() {
-        TEST_DB.createUser(TEST_USER);
+        String sender = "Lina";
+        String receiver = "Clemency";
+
+        TEST_DB.createUser(sender);
+        TEST_DB.createUser(receiver);
 
         JSONObject jobj = new JSONObject();
-        jobj.put("sender", TEST_USER);
+        jobj.put("sender", sender);
         jobj.put("msg", "DROP table adaUser CASCADE;");
 
-        TEST_DB.insert(jobj, TEST_USER);
+        TEST_DB.insert(jobj, receiver);
 
-        Assert.assertTrue(TEST_DB.checkUser(TEST_USER));
+        Assert.assertTrue(TEST_DB.checkUser(sender));
     }
 
-    @Test
-    public void insertMessage_noSender_fails() {
-        expectedException.expect(JSONException.class);
-        TEST_DB.createUser(TEST_USER);
-
-        JSONObject jobj = new JSONObject();
-        jobj.put("msg", "DROP table adaUser CASCADE");
-
-        TEST_DB.insert(jobj, TEST_USER);
-    }
-
+    //
+    //    @Test
+    //    public void insertMessage_noSender_fails() {
+    //        expectedException.expect(JSONException.class);
+    //        TEST_DB.createUser(TEST_USER);
+    //
+    //        JSONObject jobj = new JSONObject();
+    //        jobj.put("msg", "DROP table adaUser CASCADE");
+    //
+    //        TEST_DB.insert(jobj, TEST_USER);
+    //    }
+    //
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
         TEST_DB.clear();
         TEST_DB.close();
     }
