c520dbd533c174f3feb34a7baad4eab774b98a86
HDCP <theaccountname@yahoo.com>
2018-12-04 19:13:49 -0500

    Chat form

--- src/main/java/ada/gui/ClientLogin.java
+++ src/main/java/ada/gui/ClientLogin.java
@@ -1,17 +1,35 @@
 package ada.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientLogin {
     private JPanel panel1;
     private JButton buttonLogin;
     private JButton buttonRegister;
+    private JTextField textField1;
+
+    public ClientLogin() {
+        buttonLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        buttonRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Client Login");
         frame.setContentPane(new ClientLogin().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
+
         frame.setSize(300, 200);
         frame.setVisible(true);
     }
